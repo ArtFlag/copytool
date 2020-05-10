ef736d465061b2ee6586247077cc0f488b0630a4
ArtFlag <aflageul@tuta.io>
2020-05-10 11:03:06 +0200

    Use Pathlib

--- copytool/tests/test_copytool.py
+++ copytool/tests/test_copytool.py
@@ -1,104 +1,102 @@
 from copytool import check_path_params, copy_paths_to_target, get_containing_folder
-import os
 import pytest
 import shutil
 import glob
-
+from pathlib import Path
 
 @pytest.fixture()
 def create_dir(request):
-    dir = "existing_subfolder"
-    dir2 = "existing_subfolder2"
+    dir1 = Path("folder1")
+    dir2 = Path("folder2")
 
-    os.mkdir(dir)
-    os.mkdir(dir2)
+    dir1.mkdir(exist_ok=True)
+    dir2.mkdir(exist_ok=True)
 
     def del_dir():
-        shutil.rmtree(dir)
-        shutil.rmtree(dir2)
+        dir1.rmdir()
+        dir2.rmdir()
 
     request.addfinalizer(del_dir)
 
 
 @pytest.fixture()
 def create_dirs_target(request):
-    dir = "existing_subfolder"
-    subdir = "subdir"
-    dir2 = "existing_subfolder2"
-    file = "file.txt"
+    dir1 = Path("folder1")
+    subfolder = dir1 / "subfolder"
+    dir2 = Path("folder2")
+    file = dir1 / "file.txt"
 
-    os.mkdir(dir)
-    os.mkdir(os.path.join(dir, subdir))
-    os.mkdir(dir2)
-    open(os.path.join(dir, file), 'a').close()
+    subfolder.mkdir(parents=True, exist_ok=True)
+    dir2.mkdir(parents=True, exist_ok=True)
+    file.touch()
 
     def del_dir():
-        shutil.rmtree(dir)
-        shutil.rmtree(dir2)
+        shutil.rmtree(str(dir1))
+        shutil.rmtree(str(dir2))
 
     request.addfinalizer(del_dir)
 
 
 def test_get_containing_folder_unknown(create_dirs_target):
-    path = "existing_subfolder/subdir"
-    assert get_containing_folder(path) == "subdir"
+    path = "folder1/subfolder"
+    assert get_containing_folder(path) == "subfolder"
 
 
 def test_copy_path_dir(create_dirs_target):
-    paths = ("existing_subfolder", "existing_subfolder2")
+    paths = ("folder1", "folder2")
     target = "target_dir"
     copy_paths_to_target(paths, target)
     target_content = [f for f in glob.glob(target + "/**/", recursive=True)]
     shutil.rmtree(target)
-    assert "target_dir/existing_subfolder/" in target_content
-    assert "target_dir/existing_subfolder/subdir/" in target_content
-    assert "target_dir/existing_subfolder2/" in target_content
+    assert "target_dir/folder1/" in target_content
+    assert "target_dir/folder1/subfolder/" in target_content
+    assert "target_dir/folder2/" in target_content
 
 
 def test_copy_path_dir_subpath(create_dirs_target):
-    paths = ("existing_subfolder/subdir",)
+    paths = ("folder1/subfolder",)
     target = "target_dir"
     copy_paths_to_target(paths, target)
     target_content = [f for f in glob.glob(target + "/**/", recursive=True)]
     shutil.rmtree(target)
-    assert "target_dir/existing_subfolder/" not in target_content
-    assert "target_dir/subdir/" in target_content
+    assert "target_dir/folder1/" not in target_content
+    assert "target_dir/subfolder/" in target_content
 
 
 def test_copy_path_files(create_dirs_target):
-    paths = ("existing_subfolder", "existing_subfolder2")
+    paths = ("folder1", "folder2")
     target = "target_dir"
     copy_paths_to_target(paths, target)
     target_content = [f for f in glob.glob(target + "/**/*", recursive=True)]
     shutil.rmtree(target)
-    assert "target_dir/existing_subfolder/file.txt" in target_content
+    assert "target_dir/folder1/file.txt" in target_content
 
 
 def test_check_several_paths_false(create_dir):
-    dir = "existing_subfolder"
-    paths = (dir, "non_existing_subfolder")
+    dir1 = "folder1"
+    paths = (dir1, "dummy")
 
     result = check_path_params(paths)
     assert result is False
 
 
 def test_check_several_paths_true(create_dir):
-    dir = "existing_subfolder"
-    dir2 = "existing_subfolder2"
-    paths = (dir, dir2)
+    dir1 = "folder1"
+    dir2 = "folder2"
+    paths = (dir1, dir2)
     result = check_path_params(paths)
     assert result is True
 
 
 def test_check_1_path_true(create_dir):
-    dir = "existing_subfolder"
+    dir1 = "folder1"
     paths = ()
     result = check_path_params(paths)
     assert result is True
 
 
 def test_check_1_path_false():
-    dir = "non-existing_subfolder"
-    paths = dir
+    dir1 = "dummy"
+    paths = dir1
     result = check_path_params(paths)
     assert result is False
